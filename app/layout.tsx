06fea0f0b5a29e8351cd75453c0234dc5544b249
Syeda Umme Ayman <aymanshoity@gmail.com>
2024-12-12 02:52:31 +0600

    course details page loading

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+"use client"
 import "@/styles/globals.css";
 import { Metadata, Viewport } from "next";
 import clsx from "clsx";
@@ -6,30 +7,24 @@ import { siteConfig } from "@/config/site";
 import { fontSans } from "@/config/fonts";
 import { Navbar } from "@/components/Shared/navbar";
 import Footer from "@/components/Shared/footer";
+import { metadata } from "@/utils/MetaData/Metadata";
+import { usePathname } from "next/navigation";
 
-export const metadata: Metadata = {
-  title: {
-    default: siteConfig.name,
-    template: `%s - ${siteConfig.name}`,
-  },
-  description: siteConfig.description,
-  icons: {
-    icon: "/favicon.ico",
-  },
-};
 
-export const viewport: Viewport = {
-  themeColor: [
-    { media: "(prefers-color-scheme: light)", color: "white" },
-    { media: "(prefers-color-scheme: dark)", color: "black" },
-  ],
-};
+// export const viewport: Viewport = {
+//   themeColor: [
+//     { media: "(prefers-color-scheme: light)", color: "white" },
+//     { media: "(prefers-color-scheme: dark)", color: "black" },
+//   ],
+// };
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const location = usePathname()
+  const detailPage = location.includes('/course_details')
   return (
     <html suppressHydrationWarning lang="en">
       <head />
@@ -44,9 +39,14 @@ export default function RootLayout({
           <div className="">
             <div className="relative flex flex-col bg-gradient-light dark:bg-gradient-dark">
               <Navbar />
-              <main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
+
+              <main className={clsx(
+                "container mx-auto pt-16 px-6 flex-grow",
+                detailPage ? "max-w-full" : "max-w-7xl",
+              )}>
                 {children}
               </main>
+
               <div className="bg-gradient-to-r from-purple-950 via-sky-950 to-indigo-950 ">
                 <Footer />
               </div>
@@ -55,7 +55,7 @@ export default function RootLayout({
           </div>
         </Providers>
       </body>
-    </html>
+    </html >
   );
 }
 
