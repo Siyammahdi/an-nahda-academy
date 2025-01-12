94fc8acdbe6fbf69c575fd50b957388409cc30b6
Siyammahdi <siyammahdi1@gmail.com>
2025-01-12 22:10:33 +0600

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -7,13 +7,14 @@ import Discussions from "./homepage/sections/Discussions";
 // import Publications from "./homepage/sections/Publications";
 // import StudentComments from "./homepage/sections/StudentComments";
 import { Testimonials } from "./homepage/sections/Testimonials";
+import { Slider } from "@nextui-org/react";
 
 
 export default function Home() {
   return (
     <div>
       <div className="absolute mt-32">
-        {/* <Slider /> */}
+        <Slider />
       </div>
       <div className="space-y-48 max-w-7xl mx-auto mb-20">
         <Banner />
